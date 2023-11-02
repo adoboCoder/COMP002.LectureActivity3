1247bfa085f8b567150be3fd9b424026e7efe6d8
Jonathan Cruz <jcruz@2cruz.com>
2023-11-01 19:00:56 -0700

    placeholder

---
+++ conditional-code.js
@@ -0,0 +1,18 @@
+// conditional code
+console.log("conditional code");
+// if statement
+
+// if-else statement
+
+// if/else-if/else statement
+
+// nested if statements
+
+// looping statements
+console.log("looping statements");
+
+// for-loop
+
+// while-loop
+
+// do-while
