22580c9e723bef97c776db0f23dad855687408be
Jonathan Cruz <jcruz@2cruz.com>
2023-11-01 18:46:00 -0700

    first commit

---
+++ script1.js
@@ -0,0 +1,33 @@
+/* expressions, statements, and variables
+// this is a single line comment
+/* 
+this
+is
+a
+multi-line
+comment
+*/
+
+// expressions examples
+"How you doin?";
+"Today is a good day";
+"Wonderful weather we are having";
+2;
+2 + 2;
+
+
+// statements
+console.log("Hello World!");
+console.log(Math.min(2, 10, 3, 5, 7));
+
+// variables
+let myVariable = 5; // this line declares a variable called myVariable and assigns the value of 5
+console.log(myVariable * 5); // outputs the value of myVariable * 5
+myVariable = 6; // changes the value of myVariable to 6
+console.log(myVariable * 5);
+console.log("variables are case-sensitive");
+let myvariable = 100; // this is permitted since myVariable is NOT the same as myvariable
+
+const pi = 3.14;
+console.log(pi);
+// pi = 2; // you cannot change the value of a const variable after assignment
